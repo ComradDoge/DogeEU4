e2c3bb9e78162ad5cda830d17da977d1bda006d8
ComradDoge <sovietredbear@gmail.com>
2020-06-10 23:33:45 +0100

    HRE Defines Bandaid

--- common/defines/NDefines.NDiplomacy.lua
+++ common/defines/NDefines.NDiplomacy.lua
@@ -18,11 +18,23 @@ NDefines.NDiplomacy.CONDOTTIERI_MIN_DURATION = 12 -- Minimum duration for Condot
 
 
 NDefines.NDiplomacy.NUM_OF_GREAT_POWERS = 10
-NDefines.NDiplomacy.CHANGE_RIVAL_YEARS = 10 --PGG Defines 25
+NDefines.NDiplomacy.CHANGE_RIVAL_YEARS = 10 --Defines 25
 
-NDefines.NDiplomacy.SHAREMAP_PRESTIGE_TRANSFER_LOSE = -10 --PGG Defines -15
+NDefines.NDiplomacy.SHAREMAP_PRESTIGE_TRANSFER_LOSE = -10 --Defines -15
 NDefines.NDiplomacy.SHAREMAP_PRESTIGE_TRANSFER_GAIN = 10
 
-NDefines.NDiplomacy.DISHONORABLE_PEACE_MONTHS = 0 --PGG Defines 12
-NDefines.NDiplomacy.CALL_ALLY_DECLINE_PRESTIGE_PENALTY = 0 --PGG Defines -25
+NDefines.NDiplomacy.DISHONORABLE_PEACE_MONTHS = 0 --Defines 12
+NDefines.NDiplomacy.CALL_ALLY_DECLINE_PRESTIGE_PENALTY = 0 --Defines -25
 
+--HRE Emperor Bandaid
+NDefines.NDiplomacy.IMPERIAL_AUTHORITY_FROM_PRINCES = 0.05 --Defines 0.1  (vanilla:  0.1=0.21/total 0.26 , modded: 0.050 = 0.10, total 0.16) scales to threshold
+NDefines.NDiplomacy.IMPERIAL_REFORM_COST = 60  --Defines 50
+NDefines.NDiplomacy.EMPEROR_REVOKE_AUTHORITY_COST = 60	--Defines 50	
+NDefines.NDiplomacy.HRE_FOREIGN_CONTROL_PENALTY = -0.005	
+
+NDefines.NDiplomacy.HRE_AUTHORITY_JOIN_EMPIRE = 5 --Defines 10
+NDefines.NDiplomacy.HRE_AUTHORITY_LEAVE_EMPIRE = -5 --Defines -10
+NDefines.NDiplomacy.HRE_AUTHORITY_PROVINCE_JOIN = 0.5
+
+NDefines.NDiplomacy.FORCE_JOIN_HRE_AUTHORITY_PER_DEVELOPMENT = 0.05 --Defines 0.1
+NDefines.NDiplomacy.FORCE_JOIN_HRE_YEARS_BOUND_BY_TREATY = 50 
