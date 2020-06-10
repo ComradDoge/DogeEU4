bc01418214f00ee8ef80a74f93b5f8bcbfc3f1c5
ComradDoge <sovietredbear@gmail.com>
2020-06-10 02:23:22 +0100

    Defines added
    The defines have already been changed with different values.

---
+++ common/defines/NDefines.NDiplomacy.lua
@@ -0,0 +1,28 @@
+-- Only Changes to NDefines.NDiplomacy should be here.
+
+-- #arumba - espionage and covert actions suck, lets make them not suck
+NDefines.NDiplomacy.INFILTRATE_ADMINISTRATION_COST = 50
+NDefines.NDiplomacy.INFILTRATE_ADMINISTRATION_DURATION = 8
+
+-- #arumba - Support rebels sucks, lets make it not suck
+NDefines.NDiplomacy.SUPPORT_REBELS_COST = 40
+NDefines.NDiplomacy.SUPPORT_REBELS_MONEY_FACTOR = 0.5
+NDefines.NDiplomacy.SUPPORT_REBELS_EFFECT = 20
+NDefines.NDiplomacy.SUPPORT_REBELS_DURATION = 3 -- in years
+
+-- #arumba
+NDefines.NDiplomacy.SPY_NETWORK_SIEGE_EFFECT = 0.5
+
+-- #arumba - trying to encourage shorter term/immediate impact contracts
+NDefines.NDiplomacy.CONDOTTIERI_MIN_DURATION = 12 -- Minimum duration for Condottieri agreements that must be paid for in advance and that cannot be cancelled.
+
+
+NDefines.NDiplomacy.NUM_OF_GREAT_POWERS = 10
+NDefines.NDiplomacy.CHANGE_RIVAL_YEARS = 10 --PGG Defines 25
+
+NDefines.NDiplomacy.SHAREMAP_PRESTIGE_TRANSFER_LOSE = -10 --PGG Defines -15
+NDefines.NDiplomacy.SHAREMAP_PRESTIGE_TRANSFER_GAIN = 10
+
+NDefines.NDiplomacy.DISHONORABLE_PEACE_MONTHS = 0 --PGG Defines 12
+NDefines.NDiplomacy.CALL_ALLY_DECLINE_PRESTIGE_PENALTY = 0 --PGG Defines -25
+
---
+++ common/defines/NDefines.NGame.lua
@@ -0,0 +1,3 @@
+-- Only Changes to NDefines.NGame should be here.
+NDefines.NGame.MAX_CUSTOM_COUNTRIES = 50
+NDefines.NGame.ABANDON_UNION_PRESTIGE = 0 --PGG Defines -25
