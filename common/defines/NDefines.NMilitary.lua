742775aea4f4d53d58df30bc720c38fd7e22786b
ComradDoge <34968288+ComradDoge@users.noreply.github.com>
2020-09-27 23:55:31 +0100

    Nomad Plains

--- common/defines/NDefines.NMilitary.lua
+++ common/defines/NDefines.NMilitary.lua
@@ -7,7 +7,7 @@ NDefines.NMilitary.SUPPLY_DEPOT_MIL_COST = 10	-- Default 20, Mil power cost to b
 NDefines.NMilitary.SUPPLYLIMIT_BASE_MULTIPLIER = 8.0
 NDefines.NMilitary.BANNER_REINFORCE_SPEED = -0.25
 NDefines.NMilitary.NOMAD_PLAINS_SHOCK_BONUS = 0.15	
-NDefines.NMilitary.NOMAD_NON_PLAINS_SHOCK_PENALTY =	-0.10
+NDefines.NMilitary.NOMAD_NON_PLAINS_SHOCK_PENALTY =	-0.25
 NDefines.NMilitary.LEADER_GAIN_PERSONALITY_BASE_CHANCE = 22.0 --Base chance in percent for leader to gain a trait after combat (modified by tradition gained).
 
 NDefines.NMilitary.CAVALRY_COST = 20 -- from 25, to offset the worthlessness of cavalry in most armies. Hordes are somewhat nerfed in the mod eitherway.
