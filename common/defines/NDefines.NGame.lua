b5bd96674b24baf6a36afda6bce0be3f107b7edd
ComradDoge <34968288+ComradDoge@users.noreply.github.com>
2020-09-28 19:50:04 +0100

    Update NDefines.NGame.lua

--- common/defines/NDefines.NGame.lua
+++ common/defines/NDefines.NGame.lua
@@ -2,8 +2,5 @@
 
 --Subjects
 NDefines.NGame.ABANDON_UNION_PRESTIGE = -5 --Default -25, QoL change. The change in prestige for the overlord when abandoning a personal union
-<<<<<<< HEAD
 --NDefines.NGame.END_DATE = "1918.11.11"
-=======
-NDefines.NGame.END_DATE = "9999.12.12"
->>>>>>> parent of 56449b0... Road Buildings
+
