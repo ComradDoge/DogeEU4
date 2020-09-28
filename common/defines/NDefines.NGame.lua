fc41e126ab62ec213c83f3a598a150022c2e037e
ComradDoge <34968288+ComradDoge@users.noreply.github.com>
2020-09-28 13:57:21 +0100

    End date set to 9999

--- common/defines/NDefines.NGame.lua
+++ common/defines/NDefines.NGame.lua
@@ -2,3 +2,4 @@
 
 --Subjects
 NDefines.NGame.ABANDON_UNION_PRESTIGE = -5 --Default -25, QoL change. The change in prestige for the overlord when abandoning a personal union
+NDefines.NGame.END_DATE = "9999.12.12"
