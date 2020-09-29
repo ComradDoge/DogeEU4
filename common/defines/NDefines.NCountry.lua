9f3700e505a9fe1c1d2376645b8236c53eb49015
ComradDoge <34968288+ComradDoge@users.noreply.github.com>
2020-09-29 15:02:34 +0100

    Fixes and nerfs

--- common/defines/NDefines.NCountry.lua
+++ common/defines/NDefines.NCountry.lua
@@ -11,6 +11,10 @@ NDefines.NCountry.PS_REPLACE_RIVAL = 50 --Default 100
 NDefines.NCountry.NAT_FOCUS_YEARS = 15 -- Default 25
 NDefines.NCountry.NAT_FOCUS_YEARS_RANK = 5 -- Default 5
 
+--Advisor
+NDefines.NCountry.ADVISOR_COST_INCREASE_PER_YEAR = 0.0075
+NDefines.NCountry.SCRIPTED_ADVISOR_DISCOUNT = 0.25
+
 --Wars
 
 --Subjects
@@ -22,10 +26,10 @@ NDefines.NCountry.LEGITIMACY_DYNASTY_CHANGE = 50 -- Default 20
 -- For Ideas Expanded
 NDefines.NCountry.PS_BUY_IDEA = 250
 NDefines.NCountry.FREE_IDEA_GROUP_COST  = 7
-NDefines.NCountry.IDEA_TO_TECH = -0.01
+NDefines.NCountry.IDEA_TO_TECH = -0.001
 NDefines.NCountry.MAX_ACTIVE_POLICIES = 12
 NDefines.NCountry.BASE_POSSIBLE_POLICIES = 4
-NDefines.NCountry.MINIMUM_POLICY_TIME = 3
+NDefines.NCountry.MINIMUM_POLICY_TIME = 9
 NDefines.NCountry.MAX_IDEA_GROUPS_FROM_SAME_CATEGORY = 0.35 --35
 
 -- Military
