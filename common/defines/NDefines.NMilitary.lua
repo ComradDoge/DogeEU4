8cffc41052e30993af1c0c37cd8629234a060c15
ComradDoge <34968288+ComradDoge@users.noreply.github.com>
2020-11-12 00:22:38 +0000

    Supply Depot

--- common/defines/NDefines.NMilitary.lua
+++ common/defines/NDefines.NMilitary.lua
@@ -2,7 +2,8 @@
 ----- DOGE NDefines.NMilitary -----
 
 --Army
-NDefines.NMilitary.SUPPLY_DEPOT_MIL_COST = 10	-- Default 20, Mil power cost to build a supply depot in an area.
+NDefines.NMilitary.SUPPLY_DEPOT_MIL_COST = 25	-- Default 20, Mil power cost to build a supply depot in an area.
+NDefines.NMilitary.SUPPLY_DEPOT_DURATION_MONTHS = 120 -- D 60
 
 NDefines.NMilitary.SUPPLYLIMIT_BASE_MULTIPLIER = 8.0
 NDefines.NMilitary.BANNER_REINFORCE_SPEED = -0.25
