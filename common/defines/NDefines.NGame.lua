d60c83163eee816ea687a883f254d88d71f405ef
ComradDoge <34968288+ComradDoge@users.noreply.github.com>
2021-09-18 06:08:24 +0100

    Update NDefines.NGame.lua

--- common/defines/NDefines.NGame.lua
+++ common/defines/NDefines.NGame.lua
@@ -5,3 +5,4 @@ NDefines.NGame.ABANDON_UNION_PRESTIGE = -5 --Default -25, QoL change. The change
 NDefines.NGame.START_DATE = "1444.11.1"
 NDefines.NGame.END_DATE = "1914.07.28"
 
+NDefines.NEngine.EVENT_PROCESS_OFFSET = 20
