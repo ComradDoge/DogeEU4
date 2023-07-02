b163cf6c519a2dab5273ab61d5f2f1ba3ee64b2e
ComradDoge <raimundgri@gmail.com>
2023-07-02 20:20:26 +0100

    Update NDefines.NEconomy.lua

--- common/defines/NDefines.NEconomy.lua
+++ common/defines/NDefines.NEconomy.lua
@@ -14,7 +14,7 @@ NDefines.NAIEconomy.LOAN_REPAYMENT_SAVINGS_PRIORITY = 1000.0
 --Trade
 NDefines.NEconomy.COT_DOWNGRADE_PRESTIGE_COST = -5 -- Default -10, QoL change. Cost in prestige to downgrade a Center of Trade.
 NDefines.NEconomy.PIRATES_MONOPOLY_BONUS = 0.5 -- #1
-NDefines.NEconomy.TRADE_ADDED_VALUE_MODIFER = 0.03 --#0.05
+NDefines.NEconomy.TRADE_ADDED_VALUE_MODIFER = 0.02 --#0.05
 NDefines.NEconomy.TRADE_PROPAGATE_DIVIDER = 10 -- 5=1/5=20%, 10=1/10=10%
 
 --Edicts
