3e34eed4f6f2339752efceb2a9449a1870ba5a4f
ComradDoge <raimundgri@gmail.com>
2023-09-14 01:26:00 +0100

    great_project warscore contribution giganerf

--- common/defines/NDefines.NDiplomacy.lua
+++ common/defines/NDefines.NDiplomacy.lua
@@ -20,12 +20,16 @@ NDefines.NDiplomacy.NUM_OF_GREAT_POWERS = 12 -- Default 8
 NDefines.NDiplomacy.HEGEMONY_LOST_DAYS = 3650 -- 10 years
 
 --Monuments
-NDefines.NDiplomacy.GREAT_PROJECT_WARSCORE_COST_MODIFIER = 0.15	-- How much each great project multiplies a war score cost by (so 1 GP adds 33%, 2 add 66% etc)
+NDefines.NDiplomacy.GREAT_PROJECT_WARSCORE_COST_MODIFIER = 0.01	-- How much each great project multiplies a war score cost by (so 1 GP adds 33%, 2 add 66% etc)
 
 --Wars
 NDefines.NDiplomacy.DISHONORABLE_PEACE_MONTHS = 0 --Default 12. This is the thingie that forces you to wait a year before peacing out.
 NDefines.NDiplomacy.CALL_ALLY_DECLINE_PRESTIGE_PENALTY = -10 -- Default -25. Cost which you incur if you dont join an allies war.
 NDefines.NDiplomacy.AUTONOMY_WARSCORE_COST_MODIFIER = 0.5 -- Default 0.33. How much autonomy reduces score by (at 1, 50% autonomy = 50% reduction)
+
+--Warscore
+NDefines.NDiplomacy.AE_PROVINCE_CAP = 30 -- Default 30. (Cap for AE and Warscore)
+
 -- Celestial Empire
 NDefines.NDiplomacy.CELESTIAL_EMPIRE_DEFAULT_INFLUENCE = 80 -- 80, Starting value for Mandate value of a new Chinese Emperor.
 NDefines.NDiplomacy.CELESTIAL_EMPIRE_MODIFIER_THRESHOLD = 50 -- 50, Value of Mandate above which the positive Mandate Modifier is used instead of the negative one.
